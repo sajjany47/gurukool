931e5fb5e0b3d36923370703b5757a127342f713
Sunny Salgare <sunny.salgare@nextqore.com>
2023-04-24 14:53:34 +0530

    sidebar

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -2,51 +2,65 @@ import React from "react";
 import { useProSidebar, Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import { Link, Outlet } from "react-router-dom";
 import SidebarData from "../component/SidebarData";
-import styled from "styled-components";
 import * as FaIcons from "react-icons/fa";
+import { Toolbar } from "primereact/toolbar";
+import { Avatar } from "primereact/avatar";
+import { Image } from "primereact/image";
 
 const SideNav = () => {
-  const { toggleSidebar } = useProSidebar();
-  const Nav = styled.div`
-    background: #15171c;
-    height: 80px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  `;
+  const { collapseSidebar, isCollapsed } = useProSidebar();
+  const [isExpanded, setIsExpanded] = React.useState(true);
+  function handleToggle() {
+    if (isCollapsed) {
+      collapseSidebar(false);
+      setIsExpanded(true);
+    } else {
+      collapseSidebar(true);
+      setIsExpanded(false);
+    }
+  }
 
-  const NavIcon = styled(Link)`
-    margin-left: 2rem;
-    font-size: 2rem;
-    height: 80px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  `;
+  const startContent = (
+    <React.Fragment>
+      <Image
+        src="https://gurukoolmamc.in/home/images/title.png"
+        alt="Image"
+        width="150"
+      />
+    </React.Fragment>
+  );
+
+  const endContent = (
+    <React.Fragment>
+      <Avatar
+        icon="pi pi-user"
+        className="mr-2"
+        size="large"
+        style={{ backgroundColor: "#2196F3", color: "#ffffff" }}
+        shape="circle"
+      />
+    </React.Fragment>
+  );
+
   return (
     <div style={{ display: "flex", height: "100%" }}>
-      <Sidebar breakPoint="always">
+      <Sidebar
+        style={{ height: "100vh" }}
+        defaultCollapsed={true}
+        collapsed={!isExpanded}
+      >
         <Menu className="sidebar">
           <MenuItem>
-            <h1
-              style={{
-                marginTop: "60px",
-                color: "green",
-                fontSize: "40px",
+            <FaIcons.FaBars
+              onClick={() => {
+                collapseSidebar();
               }}
-            >
-              GuruKool
-            </h1>
+            />
           </MenuItem>
           {SidebarData.teacher.map((item) => {
             return (
               <>
-                <MenuItem
-                  icon={item.icon}
-                  component={<Link to={item.path} />}
-                  style={{ marginTop: "10px", color: "white" }}
-                  className="sideItem"
-                >
+                <MenuItem icon={item.icon} component={<Link to={item.path} />}>
                   {item.title}
                 </MenuItem>
               </>
@@ -55,11 +69,13 @@ const SideNav = () => {
         </Menu>
       </Sidebar>
       <main className="w-full">
-        <Nav>
-          <NavIcon to="#">
-            <FaIcons.FaBars onClick={() => toggleSidebar()} />
-          </NavIcon>
-        </Nav>
+        <div className="card">
+          <Toolbar
+            start={startContent}
+            end={endContent}
+            style={{ backgroundColor: "#e9eefb" }}
+          />
+        </div>
         <Outlet />
       </main>
     </div>
