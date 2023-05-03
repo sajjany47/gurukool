2fb4f0da15e0fd2580d964fa0545965f5ec1acf2
sajjany47 <sajjany47@gmail.com>
2023-05-04 00:10:45 +0530

    fieldComponent

--- src/Page/Teacher/Home.js
+++ src/Page/Teacher/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { Field, Form, Formik, FormikProps } from "formik";
+import { inputFiled } from "../../component/Field";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Formik
+        initialValues={{ email: "", username: "" }}
+        onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field component={inputFiled} name="email" type="email" />
+            {errors.email && touched.email ? <div>{errors.email}</div> : null}
+            <Field component={inputFiled} name="username" />
+            {errors.username && touched.username ? (
+              <div>{errors.username}</div>
+            ) : null}
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 };
 
 export default Home;
--- src/component/Field.js
+++ src/component/Field.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
 import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { classNames } from "primereact/utils";
 
-const inputFiled = (props) => {
+export const inputFiled = ({ field, form: { touched, errors }, ...props }) => {
   return (
     <>
       <span className="p-float-label">
-        <InputText id={props.name} />
-        <label htmlFor="username">{props.name}</label>
+        <InputText id={props.name} name={props.name} />
+        <label htmlFor="input_value">{props.header}</label>
       </span>
+      {touched[field.name] && errors[field.name] && (
+        <div className="error">{errors[field.name]}</div>
+      )}
     </>
   );
 };
-
-export default inputFiled;
