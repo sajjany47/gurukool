579ad8ae45e678aa313758e55e415d7770e77eb9
sajjany47 <sajjany47@gmail.com>
2023-05-07 21:14:12 +0530

    task assign

--- src/Page/Teacher/TaskAssign.js
+++ src/Page/Teacher/TaskAssign.js
@@ -3,9 +3,12 @@ import { Dropdown } from "primereact/dropdown";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { StudentData } from "../../config/Data";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
 
 const TaskAssign = () => {
-  const [selectedClass, setSelectedClass] = useState(null);
+  const [selectedClass, setSelectedClass] = useState([]);
+  const [filterClasses, setFilterClasses] = useState(StudentData);
   const classDetail = [
     { name: "Seven", code: "Seven" },
     { name: "Eight", code: "Eight" },
@@ -15,10 +18,22 @@ const TaskAssign = () => {
     { name: "Twelve", code: "Twelve" },
   ];
 
-  const assignTask = () => {
+  const handleSelectClass = (e) => {
+    setSelectedClass(e);
+    const filterClass = StudentData.filter((item) => item.class === e.name);
+    console.log(filterClass);
+    setFilterClasses(filterClass);
+  };
+
+  const handleTaskAssign = (id) => {};
+
+  const assignTask = (item) => {
     return (
       <div>
-        <i className="pi pi-send mr-2" />
+        <i
+          className="pi pi-send mr-2"
+          onClick={() => handleTaskAssign(item.id)}
+        />
         <i className="pi pi-eye mr-2" />
         <i className="pi pi-comment mr-2" />
       </div>
@@ -31,7 +46,7 @@ const TaskAssign = () => {
         <div className="col-12 md:col-6">
           <Dropdown
             value={selectedClass}
-            onChange={(e) => setSelectedClass(e.value)}
+            onChange={(e) => handleSelectClass(e.value)}
             options={classDetail}
             optionLabel="name"
             placeholder="Select a Class"
@@ -39,18 +54,14 @@ const TaskAssign = () => {
           />
         </div>
         <div className="col-12 md:col-6">
-          {/* <Dropdown
-            value={selectedCity}
-            onChange={(e) => setSelectedCity(e.value)}
-            options={cities}
-            optionLabel="name"
-            placeholder="Select a City"
-            className="w-full md:w-14rem"
-          /> */}
+          <div className="p-input-icon-left">
+            <i className="pi pi-search" />
+            <InputText placeholder="Search" />
+          </div>
         </div>
       </div>
       <div className="col-12 card">
-        <DataTable value={StudentData} tableStyle={{ minWidth: "50rem" }}>
+        <DataTable value={filterClasses} tableStyle={{ minWidth: "50rem" }}>
           <Column field="id" header="Id"></Column>
           <Column field="name" header="Name"></Column>
           <Column field="class" header="Class"></Column>
