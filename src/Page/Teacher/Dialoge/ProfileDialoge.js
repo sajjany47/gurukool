64ec203916e0499e28ff9875626015f0dd1573d2
Sunny Salgare <sunny.salgare@nextqore.com>
2023-05-08 14:36:17 +0530

    Task Assign

---
+++ src/Page/Teacher/Dialoge/ProfileDialoge.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProfileDialoge = () => {
+  return <div>ProfileDialoge</div>;
+};
+
+export default ProfileDialoge;
--- src/Page/Teacher/Dialoge/TaskDialoge.js
+++ src/Page/Teacher/Dialoge/TaskDialoge.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { InputTextarea } from "primereact/inputtextarea";
+import { FileUpload } from "primereact/fileupload";
+import { Toast } from "primereact/toast";
+import { Button } from "primereact/button";
 
 const TaskDialoge = () => {
-  return <div>TaskDialoge</div>;
+  const toast = useRef(null);
+  const [description, setDescription] = useState("");
+
+  const onUploadTask = (e) => {
+    const file = e.files[0];
+    console.log(file);
+    toast.current.show({
+      severity: "info",
+      summary: "Success",
+      detail: "File Uploaded",
+    });
+  };
+  return (
+    <>
+      <Toast ref={toast}></Toast>
+      <div className="grid">
+        <div className="col-12 md:col-6">
+          <InputTextarea
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            rows={5}
+            cols={30}
+            placeholder="Description about task"
+          />
+        </div>
+        <div className="col-12 md:col-6">
+          <FileUpload
+            mode="basic"
+            name="demo[]"
+            url="/api/upload"
+            customUpload
+            multiple
+            accept="image,pdf/*"
+            maxFileSize={1000000}
+            uploadHandler={(e) => onUploadTask(e)}
+          />
+        </div>
+        <div className="col-12 md:col-12">
+          <Button label="Submit" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TaskDialoge;
